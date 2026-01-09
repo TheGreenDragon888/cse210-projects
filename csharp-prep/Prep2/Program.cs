908d3de2511cb18064e762094e12459b799fb99e
TheGreenDragon888 <isaacday888@gmail.com>
2026-01-09 11:36:41 -0700

    Completed the W01 Prove Parts 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade_response = Console.ReadLine();
+        int grade_percentage = int.Parse(grade_response);
+
+        string grade_letter = "F";
+        if (grade_percentage >= 90) { grade_letter = "A"; }
+        else if (grade_percentage >= 80) { grade_letter = "B"; }
+        else if (grade_percentage >= 70) { grade_letter = "C"; }
+        else if (grade_percentage >= 60) { grade_letter = "D"; }
+
+        Console.Write($"Your grade is {grade_letter}.");
+        if (grade_percentage >= 70)
+        {
+            Console.Write("\nCongratulations on passing the class!");
+        }
+        else
+        {
+            Console.Write("\nYou did not pass the class. Better luck next time!");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your lsat name? ");
+        string last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
