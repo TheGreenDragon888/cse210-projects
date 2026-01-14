8886eedd231fc67e58f7e761dbd7be7b20af6fdf
TheGreenDragon888 <isaacday888@gmail.com>
2026-01-14 10:35:08 -0700

    Completed assignment prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite integer? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromptUserBirthYear(out int input_year)
+    {
+        Console.Write("What year were you born? ");
+        input_year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int input_int)
+    {
+        return input_int * input_int;
+    }
+
+    static void DisplayResult(string username, int squared_number, int birthyear)
+    {
+        Console.WriteLine($"{username}, the square of your number is {squared_number}.");
+        Console.WriteLine($"{username}, you will turn {2026 - birthyear} this year.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int favorite_number = PromptUserNumber();
+        int birthyear;
+        PromptUserBirthYear(out birthyear);
+
+        DisplayResult(username, SquareNumber(favorite_number), birthyear);
     }
 }
